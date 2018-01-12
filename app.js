dbf4f12aa5d99d3839fb257f20efeea650185f36
Cassandra <cmcorrales3@gmail.com>
2018-01-11 23:59:29 -0500

    modify function

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ function getRandomArticle() {
   console.log("click")
   window.location = "https://en.wikipedia.org/wiki/Special:Random";
 }
+
+function getRandomArticle() {
+  console.log("click")
+  window.location = "https://en.wikipedia.org/wiki/Special:Random";
+}
